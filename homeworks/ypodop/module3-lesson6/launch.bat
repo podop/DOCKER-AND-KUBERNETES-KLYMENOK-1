d10daf4e3d980f7cc630be4ee375705508cbee80
Yurii Pidopryhora <y.podop@gmail.com>
2025-11-17 17:46:34 +0200

    module3-lesson6/k8s.yaml

---
+++ homeworks/ypodop/module3-lesson6/launch.bat
@@ -0,0 +1,4 @@
+docker build -t ypodop/course-app:latest . -f homeworks/ypodop/module3-lesson6/Dockerfile
+docker push ypodop/course-app:latest
+cd homeworks/ypodop/module3-lesson6
+kubectl apply -f k8s.yaml
